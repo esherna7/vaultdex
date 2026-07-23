d0636107dca729717534d9aeeab1b9d4afe3c27c
Esai <hernandezesai123@gmail.com>
2026-07-23 15:45:11 -0500

    Initial Java + Spring Boot setup

---
+++ pokevault-api/src/main/java/io/github/esherna7/pokevault/PokevaultApplication.java
@@ -0,0 +1,13 @@
+package io.github.esherna7.pokevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokevaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokevaultApplication.class, args);
+	}
+
+}
---
+++ pokevault-api/src/test/java/io/github/esherna7/pokevault/PokevaultApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.esherna7.pokevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokevaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
